c6361b58b0677af770c5f4bd73748e79691ae868
Xangel0s <zastuto5@gmail.com>
2026-09-17 12:02:36 -0500

    Eliminar frontend - migración a CLI

--- frontend/src/hooks/index.ts
+++
@@ -1,6 +0,0 @@
-export { useKeyboard } from "./useKeyboard";
-export { useOnClickOutside } from "./useOnClickOutside";
-export { useScrollToBottom } from "./useScrollToBottom";
-export { useGlobalEmergencyHandler } from "./useEmergencyKill";
-export { useAvailableModels, type ModelOption } from "./useAvailableModels";
-export { useWakeWord } from "./useWakeWord";
--- frontend/src/types/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./chat";
-export * from "./skill";
-export * from "./connector";
-export * from "./project";
-export * from "./user";
